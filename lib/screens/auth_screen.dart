f4539c8e3d138bfb1add12dcc8993143d37a80e6
faiyaz <faiyazullah54@gmail.com>
2023-03-13 14:27:14 +0530

    completed auth form

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -10,11 +10,22 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  void _submitAuthForm({
+    required String email,
+    required String username,
+    required String password,
+    required bool isLogin,
+  }) {
+    print(email);
+    print(password);
+    print(username);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 222, 222, 222),
-      body: AuthForm(),
+      body: AuthForm(submitHelper: _submitAuthForm),
     );
   }
 }
--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
-  const AuthForm({super.key});
+  final Function({
+    required String email,
+    required String username,
+    required String password,
+    required bool isLogin,
+  }) submitHelper;
+  AuthForm({super.key, required this.submitHelper});
 
   @override
   State<AuthForm> createState() => _AuthFormState();
 }
 
 class _AuthFormState extends State<AuthForm> {
+  //form will mangage with this key
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+
+  String _userEmail = '';
+  String _userName = '';
+  String _userPassword = '';
+
+  void _trySubmit() {
+    bool isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState!.save();
+
+      //use those values to send auth to firebase
+      widget.submitHelper(
+          email: _userEmail,
+          username: _userName,
+          password: _userPassword,
+          isLogin: _isLogin);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -18,28 +48,76 @@ class _AuthFormState extends State<AuthForm> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(labelText: 'Email address'),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Username'),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Password'),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Login'),
-                ),
-                TextButton(onPressed: () {}, child: Text('Create new account?'))
-              ],
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    key: ValueKey('email'),
+                    onSaved: (value) {
+                      _userEmail = value!;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(labelText: 'Email address'),
+                    validator: (value) {
+                      if (value == null || !value.contains('@')) {
+                        return 'please enter a valid email address';
+                      }
+                      return null;
+                    },
+                  ),
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('username'),
+                      onSaved: (value) {
+                        _userName = value!;
+                      },
+                      decoration: InputDecoration(labelText: 'Username'),
+                      validator: (value) {
+                        if (value!.isEmpty || value.length < 4) {
+                          return 'Username length must be 5 or more';
+                        }
+                        return null;
+                      },
+                    ),
+                  TextFormField(
+                    key: ValueKey('password'),
+                    onSaved: (value) {
+                      _userPassword = value!;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 7) {
+                        return 'Password length must be 8 or more';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    onPressed: _trySubmit,
+                    child: Text(_isLogin ? 'Login' : 'SignUp'),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      },
+                      child: Text(_isLogin
+                          ? 'Create new account?'
+                          : 'I already have an account'))
+                ],
+              ),
             ),
           ),
         ),
